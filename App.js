33ea05c15d33d88d279a7ea302c5a6ef949591f8
Xi162 <huy.bui16bit@hcmut.edu.vn>
2021-11-10 18:12:39 +0700

    first time push

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar, useState } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, TouchableOpacity, Alert, Button, SafeAreaView } from 'react-native';
 
-export default function App() {
+
+
+const UselessTextInput = () => {
+  const [text, onChangeText] = React.useState('');
+  
+  const textHandler = () =>{
+    onChangeText(text)
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <View style = {styles.inputzone}> 
+      <TextInput style={styles.input} onChangeText={onChangeText} value={text} />
+      <Button title = "ADD" onPress ={()=> Alert.alert("Press!","Please press OK")} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    width: "80%",
+    borderWidth: 1,
+    padding: 5,
   },
+  container:{
+    flex:1,  
+    flexDirection:'column',
+    justifyContent: 'flex-end'
+  },
+  inputzone:{
+    marginBottom: "30%",
+    flexDirection: 'column',
+    justifyContent:'space-around',
+    alignItems:'center',
+  }
 });
+
+export default UselessTextInput;
